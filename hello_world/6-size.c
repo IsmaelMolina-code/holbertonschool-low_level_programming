a3bfd034ca1907ff357fd4ca5847c8c44ed70901
Super User <ismaelmolina544@gmail.com>
2023-02-15 11:12:17 -0800

    Printing the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
+int intType;
+float floatType;
+double doubleType;
+char charType;
 
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
-    return 0;
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(doubleType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+
+return (0);
 }
