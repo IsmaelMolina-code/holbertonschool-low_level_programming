6c838fe55e2bba528a00a4b1de4854453d7b44a6
Super User <ismaelmolina544@gmail.com>
2023-02-16 11:33:24 -0800

    Print digit numbers in base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int num = 0;
+
+while (num <= 9)
+{
+	printf("%d", num);
+	num++;
+}
+
+	putchar('\n');
+
+return (0);
+}
