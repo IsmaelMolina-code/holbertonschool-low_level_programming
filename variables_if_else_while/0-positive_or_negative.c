c2e996f8a26b96cf6ce33ca501349141387c5a57
Super User <ismaelmolina544@gmail.com>
2023-02-16 07:09:52 -0800

    Assigning a random number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,14 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("Your number %d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("Your number %d is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
-printf("Your number %d is negative\n", n);
+printf("%d is negative\n", n);
 
 return (0);
 }
