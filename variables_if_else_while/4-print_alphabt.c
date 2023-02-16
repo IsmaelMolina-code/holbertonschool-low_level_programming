c80bba8ca9382ab212327f3b1b86638f5402e818
Super User <ismaelmolina544@gmail.com>
2023-02-16 11:06:51 -0800

    Printing the alphabet and not q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
+
+	putchar('\n');
+
+return (0);
+}
