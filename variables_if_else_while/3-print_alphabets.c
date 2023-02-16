233fb3146cb532f8aab28203f535fe615e808def
Super User <ismaelmolina544@gmail.com>
2023-02-16 10:17:57 -0800

    Printing the alphabet in upper and lower case

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -15,15 +15,15 @@ int main(void)
 char c = 'A';
 char c1 = 'a';
 
-while (c > 64 && c <= 90)
-{
-	putchar(c);
-	c++;
-}
 while (c1 > 96 && c1 <= 122)
 {
         putchar(c1);
         c1++;
+}
+while (c > 63 && c <= 90)
+{
+        putchar(c);
+        c++;
 }
 	putchar('\n');
 
