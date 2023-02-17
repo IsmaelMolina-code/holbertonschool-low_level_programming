702a2c873b8e5e9524d5ba6bdfdae1ac7cffa22d
Super User <ismaelmolina544@gmail.com>
2023-02-17 10:55:09 -0800

    Checker for lower/uppercase character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
