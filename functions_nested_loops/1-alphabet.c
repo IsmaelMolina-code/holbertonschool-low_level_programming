bd3058e5bdf2ab43741931f9e151c260d4d4e76b
Super User <ismaelmolina544@gmail.com>
2023-02-17 08:46:00 -0800

    Function that prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+char c = 'a';
+
+while (c > 96 && c <= 122)
+{
+	_putchar(c);
+	c++;
+}
+	_putchar('\n');
+
+}
