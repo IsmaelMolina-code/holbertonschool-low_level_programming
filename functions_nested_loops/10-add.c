7b4a516b4b73ad3c213fe4efb8d105c17bbd6343
Super User <ismaelmolina544@gmail.com>
2023-02-19 08:39:29 -0800

    Adding prototypes

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		n = n * (-1);
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
