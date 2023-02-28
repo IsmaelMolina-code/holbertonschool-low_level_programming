cfe993034a1aa1c52e1814c52e5c8b321f6ff62e
Super User <ismaelmolina544@gmail.com>
2023-02-28 10:51:38 -0800

    Printing a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - function to swap numbers value.
+ *
+ * @s: the puts
+ *
+ */
+
+void print_rev(char *s)
+{
+	int leng, g;
+
+	leng = strlen(s);
+
+	for(g = leng - 1; g >= 0; g--) {
+		_putchar(s[g]);        
+	}
+	_putchar('\n');
+}
