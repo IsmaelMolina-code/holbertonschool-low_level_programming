b33edb9e2003cce7607bd76774c04bb8cf1da05b
Super User <ismaelmolina544@gmail.com>
2023-02-28 04:51:34 -0800

    Swaping int values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void swap_int - function to swap numbers value.
+ *
+ * @a: return an int
+ *
+ * @b: return another int
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
