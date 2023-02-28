a14acb63de2314b3fcbc3033b7a31cc004ee26e3
Super User <ismaelmolina544@gmail.com>
2023-02-28 06:14:18 -0800

    Counting length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to swap numbers value.
+ *
+ * @l: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(str);
+}
