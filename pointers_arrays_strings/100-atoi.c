438adafa0916dcd1bdcd92fc53488bcff2cb0126
Super User <ismaelmolina544@gmail.com>
2023-03-01 09:11:06 -0800

    Seeing

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _atoi - function that changes a string to an integer.
+ *
+ * @s: a
+ *
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, res = 0;
+
+	const char *p = s;
+
+	while (isspace(*p))
+	{
+		++p;
+	}
+
+	if (*p == '-' || *p == '+')
+	{
+		sign = (*p++ == '-') ? -1 : 1;
+	}
+
+	while (isdigit(*p))
+	{
+		res = res * 10 + (*p++ - '0');
+	}
+
+	return (sign * res);
+}
