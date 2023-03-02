91b5f7a135951ccb6ca9631b4d614a56143ce014
Super User <ismaelmolina544@gmail.com>
2023-03-02 11:19:42 -0800

    Reversing the content of an array of integers

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - function to reverse the content of an array of integers.
+ *
+ * @a: an int
+ *
+ * @n: another int
+ *
+ * Return: void _reverse_array function
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+
+	while (i < j)
+	{
+		if (a[i] != a[j])
+		{
+			int num = a[i];
+			a[i] = a[j];
+			a[j] = num;
+		}
+		i++;
+		j--;
+	}
+}
