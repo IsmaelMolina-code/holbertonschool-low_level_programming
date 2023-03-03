5ff32b3e238d31b6fc4188314b206044bff0a527
Super User <ismaelmolina544@gmail.com>
2023-03-02 16:36:16 -0800

    Capitalizing all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - function to swap numbers value.
+ *
+ * @str: the string
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int nt = 1;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (nt && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+			nt = 0;
+		} else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			nt = 1;
+		}
+	}
+
+	return (str);
+}
