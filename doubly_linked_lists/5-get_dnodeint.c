1dcad0f6505ae4a720ece278b0980fc937d1c3fd
Super User <ismaelmolina544@gmail.com>
2023-04-10 20:04:07 -0700

    Task 5 of Data Structures

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,15 @@
 #include "lists.h"
 
 /**
- *  * get_dnodeint_at_index - this function return the pointer to 9th node
- *   * end
- *    * @head: a pointer to head pointer
- *     * @index: the node index
- *      * Return: the number of nodes
- *       */
+ * get_dnodeint_at_index - this function return the pointer to 9th node
+ * end
+ *
+ * @head: a pointer to head pointer
+ *
+ * @index: the node index
+ *
+ * Return: the number of nodes
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
