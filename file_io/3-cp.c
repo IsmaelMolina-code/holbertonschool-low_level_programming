e23a0ee75ab09f596044422ae9cb3844e4255ae3
Super User <ismaelmolina544@gmail.com>
2023-04-16 13:44:55 -0700

    Task 3 of File I/O

---
+++ file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * @ac: argc
+ *
+ * @av: argv
+ *
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	char buffer[1024];
+	int fd1, fd2, nread, nwrite;
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(av[1], O_RDONLY);
+
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd1);
+		exit(99);
+	}
+
+	while ((nread = read(fd1, buffer, 1024)))
+	{
+		nwrite = write(fd2, buffer, nread);
+
+		if (nwrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
+	return (0);
+}
+
