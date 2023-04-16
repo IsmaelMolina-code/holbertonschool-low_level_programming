aafa82ddbd9cf425fdbf50913075b2661f5c5ddc
Super User <ismaelmolina544@gmail.com>
2023-04-16 12:32:16 -0700

    Task 2 of File I/O

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to the end of a file
+ *
+ * @filename: pointer to file
+ *
+ * @text_content: text to append
+ *
+ * Return: 1 at a success, -1 at a fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdopen, fdwrite, text_len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdopen = open(filename, O_WRONLY | O_APPEND);
+
+	if (fdopen == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{											while (text_content[text_len] != '\0')
+		text_len++;
+	}
+
+	fdwrite = write(fdopen, text_content, text_len);
+
+	if (fdwrite == -1)
+	{
+		close(fdopen);
+		return (-1);
+	}
+
+	close(fdopen);
+	return (1);
+}
