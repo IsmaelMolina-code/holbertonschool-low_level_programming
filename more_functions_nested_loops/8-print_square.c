1b7698d7ff96cf312f2f11d6025dc51cca587c84
Super User <ismaelmolina544@gmail.com>
2023-04-17 16:34:34 -0700

    Task 8 of more func pointers

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ *
+ * @size: size of the square
+ *
+ * This function prints a square using the '#' character. The size of the
+ * square is determined by the 'size' parameter.
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0) {
+		printf("\n");
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+			printf("#");
+
+		printf("\n");
+	}
+}
+
