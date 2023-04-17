593a4b8f3e226e17059a6414e197b508c18e2acf
Super User <ismaelmolina544@gmail.com>
2023-04-16 20:11:00 -0700

    Task 4 of Hash tables

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,32 +2,35 @@
 
 
 /**
- *  * hash_table_get - this function retreives a value associated with a key
- *   * @ht: hashtable to add into
- *    * @key: The key
- *     * Return: value associated with the element or NULL if key coudnt be found
- *      */
+ * hash_table_get - this function retreives a value associated with a key
+ *
+ * @ht: hashtable to add into
+ *
+ * @key: The key
+ *
+ * Return: value associated with the element or NULL if key coudnt be found
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-		unsigned long int index;
-			hash_node_t *tmp;
+	unsigned long int index;
+	hash_node_t *tmp;
 
-				if (ht == NULL || key == NULL)
-							return (0);
+	if (ht == NULL || key == NULL)
+		return (0);
 
-					index = key_index((const unsigned char *)key, ht->size);
-						tmp = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
 
-							if (tmp == NULL)
-										return (NULL);
-								while (strcmp(tmp->key, key) != 0)
-										{
-													tmp = tmp->next;
-														}
-									if (tmp == NULL)
-												return (NULL);
+	if (tmp == NULL)
+		return (NULL);
 
-										return (tmp->value);
+	while (strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (NULL);
+
+	return (tmp->value);
 
 }
