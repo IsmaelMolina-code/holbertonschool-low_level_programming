4ac2f95451689b3e9671ccf98a2bfa46d00ccf99
Super User <ismaelmolina544@gmail.com>
2023-04-17 14:22:56 -0700

    Task 3 of argc - argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,15 +13,16 @@
 
 int main(int argc, char *argv[])
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
-
-	printf("%i\n", mul);
+	int mul;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", mul);
 
 	return (0);
 }
