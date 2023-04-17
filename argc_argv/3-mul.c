604890576912644455b503db187f27afd0922a10
Super User <ismaelmolina544@gmail.com>
2023-04-17 14:19:32 -0700

    Task 3 of argc - argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to multiply two numbers
+ *
+ * @argc: integer
+ *
+ * @argv: dir to a table
+ *
+ * Return: 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%i\n", mul);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
