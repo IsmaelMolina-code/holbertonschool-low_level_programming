7aa5cbb0732a6db37841dd0a87b1dcdf1e636873
Super User <ismaelmolina544@gmail.com>
2023-04-17 14:06:37 -0700

    Task 1 of argc - argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program to print the number of arguments
+ *
+ * @argc: integer
+ *
+ * @argv: dir to a table
+ *
+ * Return: 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}i
