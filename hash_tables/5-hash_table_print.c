e7711502cb126799aba9595d1b8453bb3b842847
Super User <ismaelmolina544@gmail.com>
2023-04-16 20:11:16 -0700

    Task 5 of Hash tables

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -2,35 +2,41 @@
 
 
 /**
- *  * hash_table_print - this function retreives a value associated with a key
- *   * @ht: hashtable to print
- *    */
+ * hash_table_print - this function retreives a value associated with a key
+ *
+ * @ht: hashtable to print
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
-		unsigned long int index;
-			int notfirst;
-				hash_node_t *tmp;
+	unsigned long int index;
+	int notfirst;
+	hash_node_t *tmp;
 
-					if (ht == NULL)
-								return;
-						printf("{");
-							for (index = 0, notfirst = 0; index < ht->size; index++)
-									{
-												tmp = ht->array[index];
-														if (tmp != NULL)
-																	{
-																					if (notfirst == 1)
-																										printf(", ");
-																								printf("'%s': '%s'", tmp->key, tmp->value);
-																											while ((tmp = tmp->next) != NULL)
-																															{
-																																				printf(", ");
-																																								printf("'%s': '%s'", tmp->key, tmp->value);
-																																											}
-																														notfirst = 1;
+	if (ht == NULL)
+		return;
 
-																																}
-															}
-								printf("}\n");
+	printf("{");
+
+	for (index = 0, notfirst = 0; index < ht->size; index++)
+	{
+		tmp = ht->array[index];
+		if (tmp != NULL)
+		{
+			if (notfirst == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", tmp->key, tmp->value);
+
+			while ((tmp = tmp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", tmp->key, tmp->value);
+			}
+
+			notfirst = 1;
+		}
+	}
+
+	printf("}\n");
 }
