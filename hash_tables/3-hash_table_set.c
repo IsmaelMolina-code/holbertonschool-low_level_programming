c8e979f78d0401ad4d5e71077936f19413d538b6
Super User <ismaelmolina544@gmail.com>
2023-05-03 22:43:33 -0700

    Task 3 of Hash Tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,6 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(keymatch->key, key) == 0)
 		{
+			free(keymatch->value);
 			keymatch->value = strdup(value);
 			return (1);
 		}
