f3b59b9bf47996e270a1cad3dbb740361fdbff82
Super User <ismaelmolina544@gmail.com>
2023-05-06 17:37:24 -0700

    Calculator of Function Pointers project

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function to sum two integers
+ *
+ * @a : integer a
+ *
+ * @b : integer b
+ *
+ * Return: the result of the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to substract two integers
+ *
+ * @a : integer a
+ *
+ * @b : integer b
+ *
+ * Return: the result of the substraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiplicate two integers
+ *
+ * @a : integer a
+ *
+ * @b : integer b
+ *
+ * Return: the product of the multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divide two integers
+ *
+ * @a : integer a
+ *
+ * @b : integer b
+ *
+ * Return: the result of the division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to return the module of two integers
+ *
+ * @a : integer a
+ *
+ * @b : integer b
+ *
+ * Return: the result of the module between a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
