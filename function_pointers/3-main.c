4f1b314cd2dc4ccab0eb47e3715bdc2ef848cae2
Super User <ismaelmolina544@gmail.com>
2023-05-06 18:22:07 -0700

    Calculator of Function Pointers project

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+* main - function to run
+* 
+* @argc: argc
+* 
+* @argv: argv
+* 
+* Return: an integer
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+		int (*aux)(int, int);
+
+		aux = get_op_func(argv[2]);
+		if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", aux(num1, num2));
+		return (0);
+	}
+	printf("Error\n");
+	exit(98);
+}
